41b0b7169faa2bf0f0d565516a872678618f2618
Jason Zubiate <jzubiate.dev@gmail.com>
2023-12-29 13:00:53 -0800

    adds dummy data

--- data/data.js
+++ data/data.js
@@ -1,3 +1,38 @@
+export const sets = [
+  {
+    name: "Hard Rock",
+    location: "San Diego",
+    description:
+      "Get ready to be captivated by the latest addition to Selected Sessions: Aaron Hibell. With an infectious energy and a rapidly expanding social media presence, Aaron Hibell masterfully curates a dynamic fusion of House, Trance, and Techno. Immerse yourself in a cinematic journey as he takes center stage in front of the iconic church in Reykjavik, Iceland.",
+    video: "/assets/set1.mp4",
+    url: "https://www.youtube.com/watch?v=C5w3veGcSIk&t=402s&pp=ygUQd2VzdGVuZCBicm9va2x5bg%3D%3D",
+  },
+  {
+    name: "Outside Lands",
+    location: "San Francisco",
+    description:
+      "We’re delighted to bring you the latest instalment in our on-going Selected Sessions series, courtesy of the one and only Dom Dolla! One of the most industrious artists to emerge onto the international electronic music scene over the past few years, Dom Dolla’s rise to stardom has been nothing short of meteoric.",
+    video: "/assets/set2.mp4",
+    url: "https://www.youtube.com/watch?v=N1hLMBw_wpI&t=2171s",
+  },
+  {
+    name: "Rayo Verde",
+    location: "Rayo Verde",
+    description:
+      "This Session takes place in the deep south of Iceland, surrounded by hundreds of miles of black sand and mountains. We were hours from civilisation, exposed to the raw elements with nothing but each other and the breathtaking nature that surrounded us.",
+    video: "/assets/set3.mp4",
+    url: "https://www.youtube.com/watch?v=zX-sD_lnmKg&t=302s",
+  },
+  {
+    name: "Superior Ingredients",
+    location: "New York",
+    description:
+      "It has been a long journey, but we’re finally here. It is an absolute honour for us to present our first ever Selected Session with Gorgon City, as part of this brand new event series bringing together music, culture and creative output in unique locations across the world.",
+    video: "/assets/set4.mp4",
+    url: "https://www.youtube.com/watch?v=DGb6ubDUESM&t=148s&pp=ygUHY2xvb25lZQ%3D%3D",
+  },
+];
+
 export const publications = [
   {
     name: "How Westend Continues to Evolve",

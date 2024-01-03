d9d21495021d936e7e75fc8b89f593dbd1add50c
Jason Zubiate <jzubiate.dev@gmail.com>
2024-01-02 16:37:01 -0800

    adds hero gsap

--- components/Pages/Hero.tsx
+++ components/Pages/Hero.tsx
@@ -1,29 +1,47 @@
-import React from "react";
+"use client";
+
+import { useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 
 function Hero() {
+  useGSAP(() => {
+    let tl = gsap.timeline();
+    tl.from(".name", { opacity: 0, scale: 0.8 });
+    tl.from(".description", { y: 100, stagger: 0.2, ease: "power3" });
+  }, []);
+
   return (
     <section id="hero" className="h-[100dvh] w-full">
       <video
-        src="/assets/westendhero.mp4"
+        src="/assets/HeroBackground.mp4"
         autoPlay
         loop
         muted
         className="w-full h-full object-cover"
       />
-      <div className="flex flex-col absolute w-full h-full top-0 items-center justify-center gap-8">
-        <h1 className="text-bridal-health uppercase text-8xl font-bold">
-          Westend
-        </h1>
+      <div className="flex flex-col absolute w-full h-full top-0 items-center justify-center gap-6">
+        <div className="mask">
+          <h1 className="text-bridal-health uppercase text-8xl font-bold name">
+            Westend
+          </h1>
+        </div>
         <div className="flex gap-8">
-          <p className="text-bridal-health uppercase tracking-wider">
-            DJ + Music Producer
-          </p>
-          <p className="text-bridal-health uppercase tracking-wider">
-            2016-Present
-          </p>
-          <p className="text-bridal-health uppercase tracking-wider">
-            New York, New York
-          </p>
+          <div className="mask">
+            <p className="text-bridal-health uppercase tracking-wider description">
+              DJ + Music Producer
+            </p>
+          </div>
+          <div className="mask">
+            <p className="text-bridal-health uppercase tracking-wider description">
+              2016 - Present
+            </p>
+          </div>
+          <div className="mask">
+            <p className="text-bridal-health uppercase tracking-wider description">
+              New York, New York
+            </p>
+          </div>
         </div>
       </div>
       <div className="flex justify-between w-full px-12 py-8 absolute bottom-0">

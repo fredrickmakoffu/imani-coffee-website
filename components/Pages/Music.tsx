cdd58a61c3ba9aeb8360e99b2b931eed4280d3d8
Jason Zubiate <jzubiate.dev@gmail.com>
2024-01-02 16:38:07 -0800

    adds music styles

--- components/Pages/Music.tsx
+++ components/Pages/Music.tsx
@@ -22,7 +22,13 @@ function MusicCard({ name, year, url }: MusicCardProps) {
           <Image src={dirtyBlonde} height={300} width={300} alt="song_name" />
         </div>
         <div className="w-[300px] h-[300px] rounded-full object-center object-cover overflow-hidden flex items-center justify-center transition-all duration-500 album-cover-round">
-          <Image src={dirtyBlonde} height={300} width={300} alt="song_name" className="spin"/>
+          <Image
+            src={dirtyBlonde}
+            height={300}
+            width={300}
+            alt="song_name"
+            className="spin"
+          />
         </div>
       </div>
       <div className="flex justify-between p-4 w-full absolute bottom-0">
@@ -36,13 +42,22 @@ function MusicCard({ name, year, url }: MusicCardProps) {
 function Music() {
   return (
     <section id="music" className="py-20 px-12 mb-20 min-h-[100dvh]">
-      <h2 className="text-[144px] uppercase flex items-center gap-8 z-10">
-        <span className="whitespace-nowrap">New Music</span>
-        <Image src={circleIcon} alt="spinning_flower" />
-        <span className="whitespace-nowrap">New Releases</span>
-        <Image src={circleIcon} alt="spinning_flower" />
-        <span className="whitespace-nowrap">New Music</span>
-      </h2>
+      <div className="marquee">
+        <h2 className="text-[144px] uppercase marquee-content scrollX">
+          <span className="whitespace-nowrap">New Music</span>
+          <Image src={circleIcon} alt="spinning_flower" />
+          <span className="whitespace-nowrap">New Releases</span>
+          <Image src={circleIcon} alt="spinning_flower" />
+          <span className="whitespace-nowrap">New Music</span>
+        </h2>
+        <h2 className="text-[144px] uppercase marquee-content scrollX">
+          <span className="whitespace-nowrap">New Music</span>
+          <Image src={circleIcon} alt="spinning_flower" />
+          <span className="whitespace-nowrap">New Releases</span>
+          <Image src={circleIcon} alt="spinning_flower" />
+          <span className="whitespace-nowrap">New Music</span>
+        </h2>
+      </div>
       <div className="flex mt-24 justify-between gap-8">
         {songs.map((song, index) => (
           <MusicCard

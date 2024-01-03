4b9a14ad8ef33442fd2018811c076c6461487c48
Jason Zubiate <jzubiate.dev@gmail.com>
2024-01-02 16:16:36 -0800

    adds static shop

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
-import { Hero, Music, Sets, Events, Awards } from "@/components";
+import { Hero, About, Music, Sets, Events, Awards, Shop } from "@/components";
 
 export default function Home() {
   return (
     <main className="scroll-smooth">
       <Hero />
+      <About />
       <Music />
       <Sets />
       <Events />
       <Awards />
+      <Shop />
     </main>
   );
 }
--- components/index.ts
+++ components/index.ts
@@ -1,10 +1,12 @@
 import Menu from "./Menu";
 import Hero from "./Pages/Hero";
 import Header from "./Header";
+import About from "./Pages/About";
 import Music from "./Pages/Music";
 import Sets from "./Pages/Sets";
 import Events from "./Pages/Events";
 import Awards from "./Pages/Awards";
+import Shop from "./Pages/Shop";
 import Footer from "./Pages/Footer";
 
-export { Menu, Hero, Header, Music, Sets, Events, Awards, Footer };
+export { Menu, Hero, Header, About, Music, Sets, Events, Awards, Shop, Footer };

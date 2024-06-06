04dd622ee8de7c2b24b3e188fd911c1e999897c3
Jason Zubiate <jzubiate.dev@gmail.com>
2024-06-06 10:17:40 -0700

    refactor: adds tours

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,13 @@
 import { Hero, About, Music, Sets, Events, Awards, Shop } from "@/components";
 
-import { createClient } from "@/prismicio";
-import { SliceZone } from "@prismicio/react";
-import { components } from "@/slices";
-
-export  default async function Home() {
-
-
+export default async function Home() {
   return (
     <main className="scroll-smooth">
       <Hero />
       <About />
       <Music />
       <Sets />
-      {/* <Events /> */}
+      <Events />
       <Awards />
       <Shop />
     </main>

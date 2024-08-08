01bdd0ceabbe520de592e6e02e73f10de06b1048
Jason Zubiate <jzubiate.dev@gmail.com>
2024-08-08 10:54:16 -0700

    feature: adds meta pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Header, Footer } from "@/components";
 import { constructMetaData } from "@/utils/metadata";
 import LenisContext from "@/context/LenisContext";
 import { Analytics } from "@vercel/analytics/react";
+import Script from "next/script";
 
 export const metadata = constructMetaData();
 
@@ -19,6 +20,32 @@ export default function RootLayout({
           {children}
           <Footer />
           <Analytics />
+          <Script
+            id="fb-pixel"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                !function(f,b,e,v,n,t,s)
+                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window, document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+                fbq('init', '518018723905148');
+                fbq('track', 'PageView');
+              `,
+            }}
+          />
+          <noscript>
+            <img
+              height="1"
+              width="1"
+              style={{ display: "none" }}
+              src="https://www.facebook.com/tr?id=518018723905148&ev=PageView&noscript=1"
+            />
+          </noscript>
         </body>
       </html>
     </LenisContext>

1ce5fdd387c67c4c8fa41d38f5e01c1c89c62585
fredrickmakoffu <fredrickmakoffu@gmail.com>
2025-08-28 08:14:34 +0300

    -

--- utils/metadata.ts
+++ utils/metadata.ts
@@ -1,8 +1,8 @@
 import { Metadata } from "next";
 
 export function constructMetaData({
-  title = "itsthewestend",
-  description = "DJ & EDM Producer",
+  title = "Imani Coffee",
+  description = "Imani Coffee is a specialty coffee house dedicated to rare origins, mindful sourcing, and nature-inspired craft. Experience the art of coffee, from farm to cup.",
   image = "/assets/ogImage.png",
   icons = "/favicon.ico",
   noIndex = false,
@@ -30,11 +30,11 @@ export function constructMetaData({
       title,
       description,
       images: [image],
-      creator: "@itsthewestend",
+      creator: "@imani.coffee",
     },
     icons,
-    metadataBase: new URL("https://itsthewestend.com"),
-    themeColor: "#efefef",
+  metadataBase: new URL("https://imani.coffee"),
+  themeColor: "#FFFBF6",
     ...(noIndex && {
       robots: {
         index: false,
